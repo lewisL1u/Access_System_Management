a41ad321cbe2a0337941efb91cf759fd991c1c01
ocuclass05 <swang1618@gmail.com>
2016-04-14 15:11:42 -0500

    Added files via upload
    update AuthorizationReports page

---
+++ source code/Authy/AuthorizationReports.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AuthorizationReports.aspx.cs" Inherits="SAR_Helper.Pages.AuthorizationReports" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+     <div style="text-align: center; color: #1a1a4c; font-size: 24px; font-family: 'Century Gothic'">
+            Authorization Reports
+         <div style="font-size:18px">
+            <table class="table">
+
+            <tr>
+            <td>Show All</td>
+            <td>Group</td>
+            <td>Active</td>
+            </tr>
+            
+            <tr>
+             <td>
+                <asp:DropDownList ID="DropDownList_ShowAll" runat="server"  Width="100px" OnSelectedIndexChanged="DropDownList_ShowAll_SelectedIndexChanged" AutoPostBack="true">
+                <asp:ListItem Selected="True">YES</asp:ListItem>
+                <asp:ListItem>NO</asp:ListItem>
+            </asp:DropDownList>
+             </td> 
+            <td>
+             <asp:DropDownList ID="DropDownList_Group" runat="server" Width="100px" Enabled="False">
+             </asp:DropDownList>
+            </td>
+                <td>
+                <asp:DropDownList ID="DropDownList_Active" runat="server"  Width="100px">
+                <asp:ListItem Selected="True">ALL</asp:ListItem>
+                <asp:ListItem>YES</asp:ListItem>
+                <asp:ListItem>NO</asp:ListItem>
+                    
+            </asp:DropDownList>
+                    <asp:Button ID="SelectButton" Text="Generate Report" runat="server" CommandName="Select" /><br />
+                </td>
+             </tr>
+            
+            
+
+            </table>
+</div>
+            <br />
+            <br />
+         
+
+         <div>
+         <br />
+         <asp:ListView ID="ListView_AuthorizationReport" runat="server" DataKeyNames="AuthorizationID" ItemType="Authy.Models.Authy_Authorization" SelectMethod="ListView_AuthorizationReport_GetData">
+             <LayoutTemplate>
+                <table class ="table table-striped"  runat="server">
+                    <tr>
+                        <th >
+                            Name
+                        </th>
+                        <th>
+                            Contact
+                        </th>
+                        <th>
+                            Descriptian
+                        </th>
+                    </tr>
+                    <tr id="itemplaceholder" runat="server">
+                    </tr>
+            </table>
+                
+            </LayoutTemplate>
+
+             <ItemTemplate>  
+                 <div></div>
+                        <tr>
+                            <td><%#: Eval("AuthorizationName") %></td>
+                            <td><%#: Eval("AuthorizationContact") %></td>
+                            <td><%#: Eval("AuthorizationDescription") %></td>
+                        </tr>     
+            </ItemTemplate> 
+         </asp:ListView>
+         <br />
+    </div>
+    <%--<img src="Images/Page_Under_Construction.jpg" />--%>
+    <br />
+     </div>
+</asp:Content>
---
+++ source code/Authy/AuthorizationReports.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Authy.Models;
+
+namespace SAR_Helper.Pages
+{
+    public partial class AuthorizationReports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            loadGroups();
+        }
+
+        //Method retrieves data for ListView_AuthorizationReport
+        public IQueryable<Authy_Authorization> ListView_AuthorizationReport_GetData()
+        {
+
+            Authy_DataContext db = new Authy_DataContext();
+
+            //Return all authorizations in the database
+            /*
+            if (DropDownList_ShowAll.SelectedValue == "YES" && DropDownList_Active.SelectedValue == "ALL")
+            {       
+                return db.Authorizations.OrderBy(autX => autX.AuthorizationName);
+            }
+            //Return all active authorizations in the database
+            else if (DropDownList_ShowAll.SelectedValue == "YES" && DropDownList_Active.SelectedValue == "YES")
+            {
+                return db.Authorizations.Where(autX => autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+            }
+            //Returns all inactive autorizations in the database
+            else if (DropDownList_ShowAll.SelectedValue == "YES" && DropDownList_Active.SelectedValue == "NO")
+            {
+                return db.Authorizations.Where(autX => !autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+            }
+            return null;
+            */
+
+            if(DropDownList_ShowAll.SelectedValue == "YES")
+            {
+                switch (DropDownList_Active.SelectedValue)
+                {
+                    case "All":
+                        return db.Authorizations.OrderBy(autX => autX.AuthorizationName);
+                    case "YES":
+                        return db.Authorizations.Where(autX => autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+                    case "NO":
+                        return db.Authorizations.Where(autX => !autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+                    default:
+                        return null;
+                }
+            }
+            else
+            {
+                string groupName = "";
+
+                switch (DropDownList_Active.SelectedValue)
+                {
+                    case "All":
+                        return db.Authorizations.OrderBy(autX => autX.AuthorizationName);
+                    case "YES":
+                        return db.Authorizations.Where(autX => autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+                    case "NO":
+                        return db.Authorizations.Where(autX => !autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+                    default:
+                        return null;
+                }
+
+
+        }
+        //Method for change on showall list
+        protected void DropDownList_ShowAll_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList_ShowAll.SelectedValue == "YES")
+            {
+                DropDownList_Group.Enabled = false;
+            }
+            else
+            {
+                DropDownList_Group.Enabled = true;
+            }
+        }
+
+
+
+        //load group
+        public void loadGroups()
+        {
+            Authy_DataContext db = new Authy_DataContext();
+            //ListItemCollection items = new ListItemCollection { };
+            
+            List<string> grouplist = db.AuthorizationGroups.Select(autX => autX.AuthorizationGroupName).ToList();
+
+            DropDownList_Group.Items.Clear();
+            foreach (string item in grouplist)
+            {
+                DropDownList_Group.Items.Add(item);
+            }
+
+            //DropDownList_Group.Items.Add
+        }
+
+        /*
+        protected void DropDownList_Active_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Authy_DataContext db = new Authy_DataContext();
+            if (DropDownList_ShowAll.SelectedValue == "YES")
+            {
+                switch (DropDownList_Active.SelectedValue)
+                {
+                    case "ALL":
+                        ListView_AuthorizationReport.DataSource = db.Authorizations.OrderBy(autX => autX.AuthorizationName);
+                        break;
+                    case "YES":
+                        ListView_AuthorizationReport.DataSource = db.Authorizations.Where(autX => autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+                        break;
+                    case "NO":
+                        ListView_AuthorizationReport.DataSource = db.Authorizations.Where(autX => !autX.AuthorizationIsActive).OrderBy(autX => autX.AuthorizationName);
+                        break;
+                }
+                //ListView_AuthorizationReport.DataBind();
+            }
+        }*/
+
+
+
+    }
+}
